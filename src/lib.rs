cb3e860148297a72ac951e61221b71abfb2a8620
yehuthi <yehuthith@gmail.com>
2024-03-22 20:40:50 +0200

    export cache keys in lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ pub mod parse;
 pub mod keys;
 pub mod cache;
 
+pub use cache::Keys;
+
 /// Parses PEM data into a (key id, escaped key) iterator.
 pub fn parse<'a>(data: &'a mut [u8]) -> impl Iterator<Item = (&'a [u8], &'a [u8])> {
 	parse::Parse::new(data)
 }
+
